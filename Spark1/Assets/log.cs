c4eb9cc9d6b1bad56d61707ccc5c1ddbe54ab03a
GhalaAlsugair <ghalasugair@gmail.com>
2025-04-21 17:33:20 +0300

    ⏰

---
+++ Spark1/Assets/log.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; // Optional if you switch scenes on logout
+
+public class log : MonoBehaviour
+{
+    public GameObject logoutPanel;   // Assign your confirmation panel
+    public Button confirmButton;     // Assign "Log Out" button
+    public Button cancelButton;      // Assign "Cancel" button
+
+    void Start()
+    {
+        logoutPanel.SetActive(false); // Hide at start
+
+        confirmButton.onClick.AddListener(OnConfirmLogout);
+        cancelButton.onClick.AddListener(OnCancelLogout);
+    }
+
+    public void ShowLogoutPopup()
+    {
+        messageText.text = "Are you sure you want to log out? We’ll miss you!";
+        logoutPanel.SetActive(true);
+    }
+
+    void OnConfirmLogout()
+    {
+        Debug.Log("✅ Logging out...");
+        // Do your logout logic here (e.g., FirebaseAuth.SignOut())
+        SceneManager.LoadScene("LoginScene"); // Or any other scene
+    }
+
+    void OnCancelLogout()
+    {
+        logoutPanel.SetActive(false);
+    }
+}
